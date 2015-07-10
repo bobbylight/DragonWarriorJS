cfa152e342c00b51d37453a5f86a591b8f832a43
bobbylight <robert.e.futrell@gmail.com>
2015-07-09 23:28:18 -0400

    First GitHub Pages content

---
+++ demo/res/maps/collision_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<tileset name="collision" tilewidth="16" tileheight="16" spacing="1">
+   <image source="../collision.png" width="50" height="50"/>
+</tileset>
---
+++ demo/res/maps/tileset_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<tileset name="tiles" tilewidth="16" tileheight="16" spacing="1">
+   <image source="../tiles.png" width="407" height="254"/>
+</tileset>
