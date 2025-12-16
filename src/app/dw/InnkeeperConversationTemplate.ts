5a99577553a0d02334967bfdbccd63da86ccf6c4
bobbylight <robert.e.futrell@gmail.com>
2025-12-15 22:25:29 -0500

    fix: Innkeeper should check your gold

--- src/app/dw/InnkeeperConversationTemplate.ts
+++ src/app/dw/InnkeeperConversationTemplate.ts
@@ -13,15 +13,27 @@ export const innkeeperConversationTemplate = (game: DwGame, segmentArgs: Convers
     if (!segmentArgs.cost) {
         throw new Error(`No cost for the inn specified in conversation: ${JSON.stringify(segmentArgs)}`);
     }
+    const cost = segmentArgs.cost;
 
     return [
         {
             clear: false,
-            text: `Welcome to the traveler's inn. Room and board ${segmentArgs.cost} GOLD per night. Dost thou want a room?`,
+            text: `Welcome to the traveler's inn. Room and board ${cost} GOLD per night. Dost thou want a room?`,
             choices: [
-                { text: 'Yes', next: 'stay' },
+                {
+                    text: 'Yes',
+                    next: () => {
+                        const canAffordIt = game.party.gold >= cost;
+                        return canAffordIt ? 'stay' : 'cantAffordIt';
+                    },
+                },
                 { text: 'No', next: 'leave' },
             ],
+        },
+        {
+            id: 'cantAffordIt',
+            text: 'Thou hast not enough money.',
+            next: '_done',
         },
         {
             id: 'stay',
@@ -32,13 +44,13 @@ export const innkeeperConversationTemplate = (game: DwGame, segmentArgs: Convers
             text: 'I hope you had a good night.',
             action: () => {
                 game.party.replenishHealthAndMagic();
-                game.party.gold -= segmentArgs.cost!;
+                game.party.gold -= cost;
             },
         },
         { text: 'I shall see thee again.', next: '_done' },
         {
             id: 'leave',
-            text: 'I shall see thee again.',
+            text: 'Okay.\nGood-bye, traveler.',
         },
     ];
 };
